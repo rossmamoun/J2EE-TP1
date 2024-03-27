9a4bf0ce6c8f7a1217fa7a4ff615bfed7bc7830b
Mamoun ROSTANE <rossmamoun8@gmail.com>
2024-03-27 23:26:36 +0000

    first commit

---
+++ src/main/java/metier/MetierImpl.java
@@ -0,0 +1,30 @@
+package metier;
+
+import dao.IDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+@Component
+public class MetierImpl  implements IMetier {
+    @Autowired
+    @Qualifier("dao")
+    private IDao dao;
+
+        //public MetierImpl(IDao dao) {
+        //this.dao = dao;}
+        @Override
+        public double calcul() {
+            double tmp = dao.getData();
+            double res = tmp * 30 / Math.atan(tmp * 2);
+            return res;
+        }
+
+        public void setDao (IDao dao){
+            this.dao = dao;
+        }
+    }
+
+
+
